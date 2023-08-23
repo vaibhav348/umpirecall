c0aefce0b9dbd13b1fc2d837c11a13606ac16ec5
vaibhav348 <vp9724522@gmail.com>
2023-08-23 08:20:59 +0530

    change function for history

--- src/componets/header/Header.jsx
+++ src/componets/header/Header.jsx
@@ -1,15 +1,52 @@
 
 import "./header.css"
-const Header = ({handleUndu}) => {
- 
-  
-    
-  
+import hbicon from "../../assets/menu.png"
+const Header = ({ undoLastAction, runWicketData }) => {
+
+  function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
   return (
-    <header classNameName="header">
-    <h1 className='headertitle'>UMPIRE-CALL</h1>
-    <button className="undubtn" onClick={handleUndu}>⟳</button>
-   </header>
+
+    <>
+      <header classNameName="header">
+        <div className="topnav">
+          <div className="navmenu">
+            <h1 className='headertitle'>UMPIRE-CALL</h1>
+            <div className="rightnev">
+
+            <button className="undubtn" onClick={undoLastAction}>⟳</button>
+            <button className="icon" onClick={myFunction}>
+              {/* <img src={hbicon} alt="" /> */}
+            </button>
+            </div>
+          </div>
+          <div id="myLinks">
+            <div className="content">
+              <div>
+                <h3 className="history"> History</h3>
+                <div className="scroler">
+
+                <ul className="historyul">
+                  {runWicketData.map((data, index) => (
+                    <li className="historyli" key={index}>Runs  Wickets : {data.runs}({data.wickets}) | Over Ball : {data.over}.{data.ballCount}</li>
+                  ))}
+                </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+
+
+
+    </>
   )
 }
 export default Header
--- src/componets/scoreboard/ScoreBorde.jsx
+++ src/componets/scoreboard/ScoreBorde.jsx
@@ -2,67 +2,53 @@ import "./scorebord.css"
 import cimg from "../../assets/c.png";
 import Header from "../header/Header";
 import { useState } from 'react'
-import Keypad from "../noballkeypad/Noballkeypad"
+
+
+
+function KeypadPopup({ onSelectNumber, setIsPopupOpen, handleNoBallRuns }) {
+    const handleNumberClick = (number) => {
+        onSelectNumber(number);
+        handleNoBallRuns(number + 1, 0)
+        setIsPopupOpen(false)
+    };
+    return (
+        <div className="keypad">
+            <h4 style={{ textAlign: "center", fontWeight: 400, margin: "5px", fontFamily: "system-ui", color: "gray" }}>Extra Run on No-ball </h4>
+            <div className="keypad-buttons">
+                {[0, 1, 2, 3, 4, 5, 6].map((number) => (
+                    <button key={number} id="noballbtn" onClick={() => handleNumberClick(number)}>
+                        {number}
+                    </button>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+
 
 const ScoreBorde = () => {
-
-
-    
-    const [out, setOut] = useState(0);
+    const [runWicketData, setRunWicketData] = useState([]);
+    const [currentRun, setCurrentRun] = useState(0);
+    const [currentWicket, setCurrentWicket] = useState(0);
     const [ballCount, setBallCount] = useState(0);
     const [over, setOver] = useState(0)
-    const [outArray, setOutArray] = useState([])
-    const [flag, setFlag] = useState([])
-    const [keypadVisible, setKeypadVisible] = useState();
-    const [selectedValue, setSelectedValue] = useState(null);
-    const [valuesArray, setValuesArray] = useState([]);
-    const lastTenNumbers = flag.slice(-6);
-    //noballdata
-    const handleKeypadOpen = () => {
-        if (out >= 10) {
-            alert(`ALL PLAYER OUT & TARGET IS ${calculateSum()} !!!`)
-        }
-        else {
-          
-            setKeypadVisible(true);
-        }
-        // setValuesArray([...valuesArray, 1]);
-    };
-    const handleKeypadClose = () => {
-        setKeypadVisible(false);
-    };
-    const handleKeypadSave = (value) => {
-        setSelectedValue(value);
-        setValuesArray([...valuesArray, 1+value]);
-        setFlag([...flag,"NB"+value])
-    };
+    const lastTenNumbers = runWicketData.slice(-10);
+    const [selectedNumber, setSelectedNumber] = useState(null);
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
 
-    //handel OUT
-    const handelOut = () => {
-        if (out >= 10) {
-            alert(`ALL PLAYER OUT & TARGET IS ${calculateSum()} !!!`)
-        }
-        else {
-            setOut(out+1)
-            handleBowl();
-        setOutArray([...outArray, 1]);
-        setFlag([...flag,"W"])
-        }
+    const openPopup = () => {
+        setIsPopupOpen(true);
+    };
+    const handleNoBallRuns = (runs, wickets) => {
+        setBallCount(ballCount);
+        const newRunWicketData = [...runWicketData, { runs, wickets, over, ballCount }];
+        setRunWicketData(newRunWicketData);
+        setCurrentRun(currentRun + runs);
+        setCurrentWicket(currentWicket + wickets);
     }
-    const outSum = () => {
-        const sum = outArray.reduce((accumulator, currentValue) => accumulator + currentValue , 0);
-        return sum;
-    };
 
-    //handel run and ball whan all out
-    const handelBallRunOut = () => {
-        if (out >= 10) {
-            setBallCount(ballCount);
-            setValuesArray([...valuesArray]);
-            alert(`ALL PLAYER OUT & TARGET IS ${calculateSum()} !!!`)
 
-        }
-    }
 
     // handel over and ball
     const handleBowl = () => {
@@ -73,134 +59,76 @@ const ScoreBorde = () => {
             setOver(over + 1)
         }
     };
-
-    //handel runs 
-    const handelZero = () => {
-        setValuesArray([...valuesArray, 0]);
+    const updateRunWicket = (runs, wickets) => {
+        const newRunWicketData = [...runWicketData, { runs, wickets, over, ballCount }];
+        setRunWicketData(newRunWicketData);
+        setCurrentRun(currentRun + runs);
+        setCurrentWicket(currentWicket + wickets);
         handleBowl();
-        handelBallRunOut()
-        setFlag([...flag,0])
-    }
-    const handelOne = () => {
-        setValuesArray([...valuesArray, 1]);
-        handleBowl();
-        handelBallRunOut()
-        setFlag([...flag,1])
-    }
-    const handelTow = () => {
-        setValuesArray([...valuesArray, 2]);
-        handleBowl();
-        handelBallRunOut()
-        setFlag([...flag,2])
-    }
-    const handelThree = () => {
-        setValuesArray([...valuesArray, 3]);
-        handleBowl();
-        handelBallRunOut()
-        setFlag([...flag,3])
-    }
-    const handelFour = () => {
-        setValuesArray([...valuesArray, 4]);
-        handleBowl();
-        handelBallRunOut()
-        setFlag([...flag,4])
-    }
-    const handelFive = () => {
-        setValuesArray([...valuesArray, 5]);
-        handleBowl();
-        handelBallRunOut()
-        setFlag([...flag,5])
-    }
-    const handelSix = () => {
-        setValuesArray([...valuesArray, 6]);
-        handleBowl();
-        handelBallRunOut()
-        setFlag([...flag,6])
-    }
-  
-    const calculateSum = () => {
-        const sum = valuesArray.reduce((accumulator, currentValue) => accumulator + currentValue , 0);
-        return sum;
     };
-const handleUndu=()=>{
-        if(ballCount<=5 && ballCount>0){
-        valuesArray.pop();
-        flag.pop();
-        setBallCount(ballCount-1)
-    }
-        else if(over===0 && ballCount===0 ){
-        
-        alert("plese enter runs")
-    }  
-    else{
-    flag.pop();
-    valuesArray.pop();
-        setOver(over-1)
-        setBallCount(5)
-    }
-}
+
+    const undoLastAction = () => {
+        if (runWicketData.length > 0) {
+            const lastAction = runWicketData.pop();
+            setRunWicketData([...runWicketData]);
+            setCurrentRun(currentRun - lastAction.runs);
+            setCurrentWicket(currentWicket - lastAction.wickets);
+            setOver(lastAction.over)
+            setBallCount(lastAction.ballCount)
+        }
+    };
+
 
     return (
         <div className='scorecard'>
-        <Header handleUndu={handleUndu}/>
+
+            <Header undoLastAction={undoLastAction} runWicketData={runWicketData} />
 
             <div className="upper">
                 <div className="runview">
-                    <p className='runs'>Runs/Out : {calculateSum()}/{outSum()}</p>
+                    <p className='runs'>Runs/Out : {currentRun}/{currentWicket}</p>
                     <p className='over'>Over:{over}.{ballCount}</p>
-
                 </div>
-
                 <div className="middle">
                     <img className="cimg" src={cimg} alt="Cricket IMG" />
-
                 </div>
             </div>
-
             <div className="umpire">
-                
                 <p className="tenballrun">Runs of Last 10 Balls</p>
                 <section className="tenballrun">
                     {lastTenNumbers.length === 0 ? (
-                        <p>Enter first Run...</p>
+                        <p className="runoftenballs" >Enter first Run...</p>
                     ) :
                         (
-                            <div>
-                                {lastTenNumbers.join(', ')}
-                                {console.log(lastTenNumbers)}
+                            <div style={{ display: "flex" }}>
+
+                                {lastTenNumbers.map((data, index) => (
+                                    <li className="runoftenballs" key={index}> {data.runs}</li>
+                                ))}
                             </div>
                         )}
+
                 </section>
-               <div className="typeofball">
-                    <div className="noballhandler">
-                        {keypadVisible && (
-                            <Keypad
-                                onClose={handleKeypadClose}
-                                onSave={handleKeypadSave}
-                            />
-                        )}
-                    </div>
-                    <button className='decision-btn-noball' onClick={handleKeypadOpen}> no-ball
-
-
-                    </button>
-                    <button className='decision-btn' onClick={handelOut}> out
-                    </button>
-                    <button className='decision-btn' onClick={handelZero}> 0</button>
-                    <button className='decision-btn' onClick={handelOne}> 1</button>
-                    <button className='decision-btn' onClick={handelTow}> 2</button>
-                    <button className='decision-btn' onClick={handelThree}> 3</button>
-                    <button className='decision-btn' onClick={handelFour}> 4</button>
-                    <button className='decision-btn' onClick={handelFive}> 5</button>
-                    <button className='decision-btn' onClick={handelSix}> 6</button>
-                </div>
-
-
-
             </div>
 
+            <div className="typeofball">
+                <div className="noballhandler">
+                    {isPopupOpen && <KeypadPopup onSelectNumber={setSelectedNumber}
+                        setIsPopupOpen={setIsPopupOpen} handleNoBallRuns={handleNoBallRuns} />}
+                </div>
+                <button className='decision-btn-noball' onClick={openPopup}>No-Ball</button>
+                <button className='decision-btn-noball' onClick={() => updateRunWicket(0, 1)}>Wicket</button>
+                <button className='decision-btn-noball' onClick={() => updateRunWicket(0, 0)} value={0}> 0</button>
+                <button className='decision-btn-noball' onClick={() => updateRunWicket(1, 0)} value={1}>1</button>
+                <button className='decision-btn-noball' onClick={() => updateRunWicket(2, 0)} value={2}>2</button>
+                <button className='decision-btn-noball' onClick={() => updateRunWicket(3, 0)} value={3}>3</button>
+                <button className='decision-btn-noball' onClick={() => updateRunWicket(4, 0)} value={4}>4</button>
+                <button className='decision-btn-noball' onClick={() => updateRunWicket(5, 0)} value={5}>5</button>
+                <button className='decision-btn-noball' onClick={() => updateRunWicket(6, 0)} value={6}>6</button>
+            </div>
         </div>
+
     )
 }
 
-export default ScoreBorde;
+export default ScoreBorde
